9a36f5acb4a85b1cf215c01b6b277f20f2af4b77
Peter DeMartini <peterdemartini@me.com>
2014-01-19 14:28:56 -0800

    WSGI

---
+++ migrations/versions/502f24f8e44f_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 502f24f8e44f
+Revises: 377960afb37
+Create Date: 2014-01-19 13:52:03.276709
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '502f24f8e44f'
+down_revision = '377960afb37'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
